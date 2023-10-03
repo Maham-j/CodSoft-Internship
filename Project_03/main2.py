8985fc896d9705cb7987e47cf5fb834e6583c744
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-10-03 23:44:15 +0500

    Create main2.py

---
+++ Project_03/main2.py
@@ -0,0 +1,64 @@
+from random import*
+
+Rock = '''   
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+
+Paper = '''   
+    _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+
+
+Scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+images = [Rock ,Paper, Scissors]
+user = int(input())
+if user >=3 or user <0:
+    print('you Typed an Invalid number.You loose!')
+else:
+    print(f'{images[user]}')
+    
+    computer = randint(0,2)
+    index = images[computer]
+    print(f'Computer choose {index}')
+
+    if user == 0 and computer == 1:
+        print('You Loose')
+    elif user == 1 and computer == 0:
+        print('You Won')
+    elif user == 0 and computer == 2:
+        print('You Won')
+    elif user == 2 and computer == 0:
+        print('You Loose')
+    elif user == 1 and computer == 2:
+        print('You Loose')
+    elif user == 2 and computer == 1:
+        print('You Won')
+    elif user == computer:
+        print('You draw')
+
+
+
+
+
+
